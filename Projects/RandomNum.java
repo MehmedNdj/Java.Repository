4e9b386654d3e766a0ec73309c5f007a2291035e
MehmedNdj <mehmednedjatin13@gmail.com>
2024-03-13 15:46:54 +0200

    commit

---
+++ Projects/RandomNum.java
@@ -0,0 +1,12 @@
+package Projects;
+
+public class RandomNum {
+    //generates a random number from 0 to 100
+    int randomNum = (int)(Math.random() * 101);
+
+    public static void main(String[] args){
+
+        RandomNum random = new RandomNum();
+        System.out.print(obj.randomNum);
+    }
+}
