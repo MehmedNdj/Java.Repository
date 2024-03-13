3c15ab25f268f0ce11ffb89321061f40f8e740e1
MehmedNdj <mehmednedjatin13@gmail.com>
2024-03-13 15:29:55 +0200

    Second commit

---
+++ Projects/Variables.java
@@ -0,0 +1,14 @@
+package Projects;
+
+public class Variables {
+    static int a = 4; //This is and integer. Stores whole numbers without decimals.
+    static double b = 4.5; //This is double. It Stores only point numbers with decimals.
+    static boolean bool = false; //This stores value with two states. True or False .
+    static char name = 'C'; // This is char variable , it can hold the value of characters
+    static String MyName = "Asan"; //Stores text such as "Hello World"
+
+    public static void main(String[] args){
+        System.out.println(a + " " + b + " " + name + " " + bool + " " + MyName );
+    }
+
+}
